39e16270729d8f7ae438a939197c636d60fae72b
Jason Ody <jasonody@users.noreply.github.com>
2015-08-18 18:39:50 -0700

    Implemented revealing module pattern and wrapped in an IIFE

--- src/BatchTimer.js
+++ src/BatchTimer.js
@@ -1,33 +1,41 @@
-function BatchTimer () { }
-
-BatchTimer._tasks = [];
-BatchTimer.MIN_INTERVAL = 50;
-
-BatchTimer.addTask = function (task, interval) {
-
-	var task = {
-		operation: task,
-		interval: interval,
-		nextRun: +new Date() + interval
-	};
-
-	BatchTimer._tasks.push(task);
-
-	return function () { 
-
-		var index = BatchTimer._tasks.indexOf(task);
-		if (index >= 0) {
-			BatchTimer._tasks.splice(index, 1);
-		}
-	};
-};
-
-BatchTimer.count = function () {
-
-	return BatchTimer._tasks.length;
-};
-
-BatchTimer.reset = function () {
+window["BatchTimer"] = (function () {
 	
-	BatchTimer._tasks = [];
-};
+	return {
+		addTask: addTask,
+		count: count,
+		reset: reset
+	};
+	
+	var MIN_INTERVAL = 50;
+	var tasks = [];
+	
+	function addTask (task, interval) {
+
+		var task = {
+			operation: task,
+			interval: interval,
+			nextRun: +new Date() + interval
+		};
+
+		tasks.push(task);
+
+		return function () { 
+
+			var index = tasks.indexOf(task);
+			if (index >= 0) {
+				tasks.splice(index, 1);
+			}
+		};
+	}
+	
+	function count () {
+		
+		return tasks.length;
+	}
+	
+	function reset () {
+		
+		tasks = [];
+	}
+	
+}());
